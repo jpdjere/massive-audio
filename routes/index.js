17c9112bcf1f422f7c6cf37d4369fe58701fd865
Juan Pablo Djeredjian <jpdjeredjian@gmail.com>
2017-07-10 15:36:32 -0300

    Updated various files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var https = require('https');
+var request = require('request');
+var path = require('path');
+var fs = require('fs');
+
+var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+var natural_language_understanding = new NaturalLanguageUnderstandingV1({
+  username: '87785b96-9165-4d05-b0d3-c1c845464aa8',
+  password: 'wqtO3URm1Kzx',
+  version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
+});
+
+// Imports the Google Cloud client library
+const Language = require('@google-cloud/language');
+// Instantiates a client
+const language = Language();
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname, '../', 'views', 'index2.html'));
 });
 
 require('dotenv').config({silent: true});
@@ -13,9 +31,8 @@ const projectId = process.env.GCLOUD_PROJECT;
 
 // The audio file's encoding, sample rate in hertz, and BCP-47 language code
 const options = {
-    //encoding: 'FLAC',
+    // encoding: 'ogg',
     //sampleRateHertz: 48000,
-    maxAlternatives: 3,
     languageCode: 'es-AR'
 };
 
@@ -34,31 +51,214 @@ router.get('/api/google-speech-to-text', function (req, res) {//solo para prueba
     // Imports the Google Cloud client library
     const Speech = require('@google-cloud/speech');
 
-// Instantiates a client
+    // Instantiates a client
     const speechClient = Speech({
         projectId: projectId
     });
 
-// The name of the audio file to transcribe
-//    const fileName = './resources/audio.raw';
-    // const fileName = 'gs://eminent-augury-146415.appspot.com/leydetierras.flac';
-    const fileName = 'gs://clarin-videos/leydetierras.flac';
+    const storage = require('@google-cloud/storage');
+    const gcs = storage({
+      projectId: projectId
+    });
 
+    let encoded_url_address = req.query.url_address;
+    let unencoded_url_address = decodeURIComponent(encoded_url_address);
+    // The name of the audio file to transcribe
+    const fileName = 'gs://clarin-videos/audio.flac';
+    /*
+    let bucket_name = "clarin-videos";
 
-// Detects speech in the audio file
-    speechClient.startRecognition(fileName, options)
-            .then((results) => {
+    // let source_file_name = "";
+    let destination_blob_name = "Audios/audiotest.flac"
+    // let url_address = "https://www.youtube.com/watch?v=XBsdkAzB4qQ";
+    let url_address = req.query.url_address;
+    url_address = encodeURIComponent(url_address);
+    console.log("url_address");
+    console.log("url_address");
+    console.log("url_address");
+    console.log(url_address);
 
-            	const operation = results[0];
-            	return operation.promise();
+    let req_address = "http://cognitiva-video-converter.mybluemix.net/upload?bucket_name="+bucket_name+"&url_address="+encodeURIComponent(url_address);
+    console.log(req_address);
+
+    //Tiro mi video al servicio de Python para extraer audio, convertirlo y subirlo a GCS
+    request
+      // .get(req_address)
+      .get(`https://127.0.0.1:5000/upload?bucket_name=${bucket_name}&url_address=${url_address}`)
+      .on('error', function(err) {
+        console.log(err);
+      })
+      .on('response', function(response) {
+        console.log(response.statusCode) // 200
+        console.log(response.headers['content-type']) // 'image/png'
+        console.log(JSON.stringify(response,null,2));
+
+        speechRecognition()
+          .then(() => {
+            console.log("Termino todo.");
+          })
+      })
+      */
+
+      let audioFileName = 'public/audios/audio-'+Date.now()+'.aac';
+      const ytdl = require('ytdl-core');
+
+      let extractAudio = (url) => {
+        return new Promise(
+          (resolve, reject) => {
+
+            let youtube = ytdl(url,{
+              filter: "audioonly"
+            }).pipe(fs.createWriteStream(audioFileName));
+
+            youtube.on('end',function() {
+              console.log("Finished extracting audio");
+              resolve();
             })
+
+
+          })
+
+      }
+
+      let bucket = gcs.bucket('clarin-videos');
+      let uploadGCS = () => {
+        console.log("Entered GCS function");
+        bucket.upload(audioFileName, function(err, file) {
+          if (!err) {
+            console.log(`Uploaded ${file} to GCS`);
+          }else{
+            console.log("There was an error uploading the file");
+            console.log(err);
+          }
+
+        });
+
+      }
+
+      extractAudio(unencoded_url_address)
+        .then(() => {
+          uploadGCS();
+        })
+
+
+
+
+
+
+
+
+
+    let googleNLUPromise = (text) => {
+      return new Promise(
+        (resolve, reject) => {
+
+          // Instantiates a Document, representing the provided text
+          const document = language.document({ content: text});// Detects entities in the document
+          document.detectEntities()
             .then((results) => {
-            	const transcription = results[0]
-            	console.log(`Transcription: ${transcription}`);
+              const entities = results[1].entities;
+
+              console.log('Entities:');
+              console.log(entities);
+
+              // entities.forEach((entity) => {
+              //   console.log(entity.name);
+              //   console.log(` - Type: ${entity.type}, Salience: ${entity.salience}`);
+              //   if (entity.metadata && entity.metadata.wikipedia_url) {
+              //     console.log(` - Wikipedia URL: ${entity.metadata.wikipedia_url}$`);
+              //   }
+              // });
+              resolve(entities);
             })
             .catch((err) => {
-                console.error('ERROR:', err);
+              console.error('ERROR:', err);
             });
+
+        }
+      )
+    }
+
+
+    let watsonNLUPromise = (parameters) => {
+      return new Promise(
+        (resolve, reject) => {
+
+          natural_language_understanding.analyze(parameters, function(err, response) {
+            console.log(" ");
+            console.log("Entre a NLU:");
+            if (err){
+              console.log('error:', err);
+            }
+            else{
+              console.log(JSON.stringify(response, null, 2));
+              resolve(response);
+            }
+          });
+
+        }
+      )
+    }
+
+
+    let speechRecognition = () => {
+      console.log("entre a speech recognition");
+      let analyzedText = {};
+      return new Promise(
+        (resolve, reject) => {
+
+          // Detects speech in the audio file
+          speechClient.startRecognition(fileName, options)
+          .then((results) => {
+
+            const operation = results[0];
+            return operation.promise();
+          })
+          .then((results) => {
+            const transcription = results[0]
+            console.log(`Transcription: ${transcription}`);
+            let parameters = {
+              'text': transcription,
+              'features': {
+                'entities': {
+                  'emotion': true,
+                  'sentiment': false
+                },
+                'keywords': {
+                  'emotion': true,
+                  'sentiment': false
+                },
+                'concepts': {
+                  'emotion': true,
+                  'sentiment': false
+                }
+              }
+            }
+            googleNLUPromise(parameters.text)
+              .then((results) => {
+                analyzedText.google = results;
+                console.log("Inside Google Promise:");
+                console.log(analyzedText);
+                watsonNLUPromise(parameters)
+                .then((response) => {
+                  analyzedText.watson = response;
+                  console.log("Inside NLU Promise:");
+                  console.log(analyzedText);
+                  res.send(analyzedText);
+                });
+              })
+
+          })
+          .catch((err) => {
+            console.error('ERROR:', err);
+          });
+
+        }
+      )
+    }
+
+
+
 
 });
 
